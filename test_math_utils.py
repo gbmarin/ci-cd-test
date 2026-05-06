1a42153818b9becf4b24de511b6a31442fe82a40
Oscar Marin <oscarxmarin@gmail.com>
2026-05-06 11:42:32 -0600

    Update subtract tests

--- test_math_utils.py
+++ test_math_utils.py
@@ -14,3 +14,12 @@ def test_add_mixed():
     
 def test_subtract():
     assert subtract(5, 3) == 2
+    
+def test_subtract_zeros():
+    assert subtract(0, 0) == 0
+
+def test_subtract_negative():
+    assert subtract(-1, -1) == 0
+
+def test_subtract_mixed():
+    assert subtract(5, -3) == 8
