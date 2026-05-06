5471020c1ae387146d369c7668b17683c9e014cf
Oscar Marin <oscarxmarin@gmail.com>
2026-05-06 11:11:51 -0600

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -2,3 +2,12 @@ from math_utils import add
 
 def test_add():
     assert add(2, 3) == 5
+
+def test_add_zeros():
+    assert add(0, 0) == 0
+
+def test_add_negative():
+    assert add(-1, -1) == -2
+
+def test_add_mixed():
+    assert add(-1, 5) == 4
